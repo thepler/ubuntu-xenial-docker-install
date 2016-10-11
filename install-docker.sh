73067fecd7f64d38608bcaff6ef33c1cef93c72f
Todd Hepler <thepler@gmail.com>
2016-10-11 15:04:14 +0000

    no sudo (assume root), fiddled

--- install-docker.sh
+++ install-docker.sh
@@ -2,13 +2,21 @@
 
 # https://docs.docker.com/engine/installation/linux/ubuntulinux/
 
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-get install docker
-sudo usermod -aG docker ubuntu
-sudo groupadd docker
-sudo gpasswd -a ${USER} docker
-sudo newgrp docker
-sudo service docker restart
+set -x
+
+apt-get update
+apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get purge lxc-docker
+apt-cache policy docker-engine
+apt-get install -y docker-engine
+service docker start
+apt-get install -y docker
+docker run hello-world
+groupadd docker
+usermod -aG docker ubuntu
+gpasswd -a ${USER} docker
+service docker restart
 
